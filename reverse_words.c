c14621bd096ea46dbca0ac669c7c29d08e48257f
Aswin Prakash <aswinprakashdx@gmail.com>
2024-11-28 08:47:35 +0530

    reverse_words.c

--- reverse_words.c
+++ reverse_words.c
@@ -3,45 +3,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse(char *s);                      //function prototype
+void reverse(char *,int );
 
 int main()
 {
-    int i,j=0,k=0;
-    char word[20][20],str[30];
+    int i,l=0;
+    char str[30],temp;
     printf("Enter the sentance\n");
     scanf("%[^\n]",str);                               //reading input from user
     for(i=0;i<=strlen(str);i++)                        //getting each word from sentance
     {
-        if(str[i]==' ' || str[i]=='\0')               //checking if the character is space or null character
+        if(str[i]==' ' || str[i]=='\0')
         {
-            word[j][k]='\0';                            //storing null at the end to conver to a word
-            j++;
-            k=0;
+            reverse(&str[i-l],l);                     //passing string position and word length
+            l=0;                                      //updating length to zero
         }
         else
         {
-            word[j][k]=str[i];                        //storing characters
-            k++;
+            l++;                                      //incrementing length
         }
-    }
-    for(i=0;i<j;i++)
-    {
-        reverse(word[i]);                            //calling function to reverse
-    }
+    } 
+    printf("%s ",str);                                //printing reverse string
 }
 
-/*function to reverse a string */
 
-void reverse(char *s)              
+/*Function to reverse the word */
+void reverse (char *s,int l)
 {
-    int l=strlen(s),i=0;
     char temp;
+    int i;
     for(i=0;i<l/2;i++)
     {
-        temp=s[i];                       //storing first character in temp
-        s[i]=s[l-i-1];                   //storing last character in first index
-        s[l-i-1]=temp;                   //storing first character in last index
+        temp=s[i];                                       //stroring first character in temp
+        s[i]=s[l-i-1];                                   //stroring last character in first position
+        s[l-i-1]=temp;                                   //storing first character in last position
     }
-    printf("%s ",s);
 }
+
