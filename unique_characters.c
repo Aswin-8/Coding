8ae2b219e94639fb9441954b6cfd58376fd64e8c
Aswin Prakash <aswinprakashdx@gmail.com>
2024-12-02 17:47:32 +0530

    unique_characters.c

--- unique_characters.c
+++ unique_characters.c
@@ -6,13 +6,13 @@
 int main()
 {
     char str[20];
-    int i=0,j=0,flag=0;
+    int i=0,j=1,flag=0;
     scanf("%s",str);                               //reading the word from user
     while(str[i]!='\0')
     {
-        while(str[j+i+1]!='\0')
+        while(str[j+i]!='\0')
         {
-            if(str[i]==str[j+i+1])                  //comparing each character in string
+            if(str[i]==str[j+i])                  //comparing each character in string
             {
                 flag=1;                            //if found repeating updating flag
                 break;                             //exiting from inner loop if characters are repeating
@@ -22,7 +22,7 @@ int main()
         if(flag)                                  //if flag is true exiting from outer loop
             break;
         i++;                                      //incrementing i
-        j=0;                                      //resetting j
+        j=1;                                      //resetting j
     }
     flag?printf("False"):printf("True");
     return 0;
